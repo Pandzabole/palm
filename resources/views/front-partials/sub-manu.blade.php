063da7265ae3f69cb37ec2fee8d57f348274ba78
Predrag Pandza <predrag@thinkbig.dev>
2022-02-24 15:56:21 +0100

    WIP

--- resources/views/front-partials/sub-manu.blade.php
+++ resources/views/front-partials/sub-manu.blade.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container container-single-class">
     <div class="clearfix row">
         <div class="col-xl-12 col-lg-12 col-mb-4 col-sm-12 col-xs-12 hidden-sm-down hidden-md-down">
             <div class="b-header_nav">
@@ -7,16 +7,18 @@
                         <ul class="categories pl-0 mb-0 list-unstyled">
                             <!-- Mega menu -->
                             <!-- Top level items -->
-                            <li class=" b-has_sub b-dropdown_wrapper from-bottom online-class-bg">
-                                <a href="index.html" class=" description ">
-                                    <span class="top online-class-color">{{__('home-page.on-line')}}</span></a>
-                                <!-- Sub Menu items -->
-                            </li>
+                            @if($session === 'database-en')
+                                <li class=" b-has_sub b-dropdown_wrapper from-bottom online-class-bg">
+                                    <a href="index.html" class=" description ">
+                                        <span class="top online-class-color">{{__('home-page.on-line')}}</span></a>
+                                    <!-- Sub Menu items -->
+                                </li>
+                            @endif
                             <!-- Arts and Entertanemet -->
                             @foreach($mainCategories as $key => $mainCategory)
                                 <li class=" b-has_sub b-dropdown_wrapper from-bottom">
                                     <a href="shop-grid-three.html" class=" description ">
-                                        <span class="top">{{ $mainCategory->name }} </span><i
+                                        <span class="top"> {{ $mainCategory->name }} </span><i
                                             class="fa fa-angle-down"></i></a>
                                     <!-- Sub Menu items -->
                                     @foreach($mainCategory->classSubCategory as $classSub)
@@ -25,11 +27,16 @@
                                                 <div class="dropdown-inner">
                                                     <div class="row">
                                                         @foreach($mainCategory->classSubCategory as $classSub)
-                                                            <div class="col-xs-12 col-sm-4">
+                                                            <div class="col-xs-12
+                                                                @if(count($mainCategory->classSubCategory) == 1) col-sm-12
+                                                                @elseif(count($mainCategory->classSubCategory) == 2) col-sm-6
+                                                                @else col-sm-4
+                                                                    @endif">
                                                                 <div class="menu-item">
                                                                     <div class="categories">
                                                                         <div class="row">
-                                                                            <div class="col-sm-12 hover-menu">
+                                                                            <div class="col-sm-12 hover-menu @if($session === 'database-om' || $session === 'database-ar')
+                                                                                text-right @else text-left @endif">
                                                                                 <ul>
                                                                                     <li>
                                                                                         <a href="{{ route('sub-classes', $classSub->uuid) }}">{{$classSub->name}}</a>
@@ -48,6 +55,14 @@
                                     @endforeach
                                 </li>
                             @endforeach
+
+                            @if($session === 'database-om' || $session === 'database-ar')
+                                <li class=" b-has_sub b-dropdown_wrapper from-bottom online-class-bg">
+                                    <a href="index.html" class=" description ">
+                                        <span class="top online-class-color">{{__('home-page.on-line')}}</span></a>
+                                    <!-- Sub Menu items -->
+                                </li>
+                            @endif
                         </ul>
                     </div>
                 </div>

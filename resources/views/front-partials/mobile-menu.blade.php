8ea8d9d1bd121cd0f9f0028f88bae98ea51d0184
Predrag Pandza <predrag@thinkbig.dev>
2022-03-29 21:06:02 +0200

    Add live search on mobile

--- resources/views/front-partials/mobile-menu.blade.php
+++ resources/views/front-partials/mobile-menu.blade.php
@@ -7,10 +7,18 @@
 @endif
     <ul class="mobile-top">
         <li class="search">
-            <div class="search-holder-mobile">
-                <input type="text" name="search-mobile" value="" placeholder="Search" class="form-control">
-                <a class="fa fa-search"></a>
-            </div>
+            <form role="search" method="get" id="searchform-mobile" class="searchform-mobile  basel-ajax-search" action="" data-thumbnail="1"
+                  data-price="1" data-count="3">
+                @csrf
+                    <label class="screen-reader-text" for="s"></label>
+                    <input type="text" placeholder="Search for classes" value="" name="search" id="search-mobile" autocomplete="off">
+            </form>
+        </li>
+        <li id="results-mobile">
+            <p></p>
+        </li>
+        <li class="no-search-results-mobile" id="no-search-mobile">
+            No search results
         </li>
     </ul>
     <ul class="categories @if($session === 'database-om' || $session === 'database-ar') text-right @else text-center @endif ">
@@ -63,3 +71,51 @@
 </div>
 
 @yield('mobile-menu')
+    @section('js-links')
+        @parent
+        <script>
+            $(document).ready(function () {
+                $('.btn-close-mobile-menu').on('click', function () {
+                    $('#no-search-mobile').hide();
+                    $('#search-mobile').val('');
+                    $('.search-result-mobile').empty();
+                });
+
+                $('#no-search-mobile').hide()
+
+                $('#search-mobile').on('keyup', function (e) {
+                    e.preventDefault()
+                    let search = $(this).val();
+                    if (search.length === 0) {
+                        $('.search-result-mobile').empty();
+                    }
+                    $.ajax({
+                        url: "{{ route('search-class') }}",
+                        search: search,
+                        data: {
+                            search: search,
+                            _token: "{{ csrf_token() }}"
+                        },
+                        type: "GET",
+                        success: function (data) {
+                            if (data) {
+                                $('.search-result-mobile').empty();
+                                $('#no-search-mobile').hide();
+                                $.each(data.classes, function (index, subcategory) {
+
+                                    let url = '{{  route('single-class', ['lang' => $selectedLanguageLayout, ":id"]) }}';
+                                    url = url.replace(':id', subcategory.uuid);
+
+                                    $('#results-mobile').append('<div class="search-result-mobile"><a href="' + url + '"> '  + subcategory.name + '</a> </div>');
+                                })
+                            }
+                            if (data.classes.length === 0) {
+                                $('.search-result-mobile').empty();
+                                $('#no-search-mobile').show()
+                            }
+                        }
+                    });
+                })
+            })
+        </script>
+    @endsection
